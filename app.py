92e8d32a5bb309e51af07e120907bee5c7353988
Jackson Sheppard <sheppard@slac.stanford.edu>
2021-01-28 20:39:17 -0800

    MNT: flask setup for hawaii sqlite

--- app.py
+++ app.py
@@ -1,4 +1,16 @@
-from flask import Flask
+import datetime as dt
+import numpy as np
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+engine = create_engine("sqlite:///hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
 
 app = Flask(__name__)
 
