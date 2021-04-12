eeb4b27bb48c1c9113865aee55c3e9856f7c7a1e
Jackson Sheppard <sheppard@slac.stanford.edu>
2021-04-12 15:55:16 -0700

    STY: additional comments and consistent use of quotes

--- app.py
+++ app.py
@@ -19,19 +19,21 @@ session = Session(engine)
 
 app = Flask(__name__)
 
+# Index Route
 @app.route('/')
 def welcome():
     return(
-    '''
+    """
     Welcome to the Climate Analysis API!<br/>
     Available Routes:<br/>
     /api/v1.0/precipitation<br/>
     /api/v1.0/stations<br/>
     /api/v1.0/tobs<br/>
     /api/v1.0/temp/start/end<br/>
-    '''
+    """
     )
 
+# Precipitation Route
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
@@ -40,21 +42,24 @@ def precipitation():
     precip = {date: prcp for date, prcp in precipitation}
     return jsonify(precip)
 
+# Stations Route
 @app.route("/api/v1.0/stations")
 def stations():
     results = session.query(Station.station).all()
     stations = list(np.ravel(results))
     return jsonify(stations=stations)
 
+# All Temperatures Route
 @app.route("/api/v1.0/tobs")
 def temp_monthly():
     prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
     results = session.query(Measurement.tobs).\
-        filter(Measurement.station == 'USC00519281').\
+        filter(Measurement.station == "USC00519281").\
         filter(Measurement.date >= prev_year).all()
     temps = list(np.ravel(results))
     return jsonify(temps=temps)
 
+# Temperatures Over Range Route
 @app.route("/api/v1.0/temp/<start>")
 @app.route("/api/v1.0/temp/<start>/<end>")
 def stats(start=None, end=None):
